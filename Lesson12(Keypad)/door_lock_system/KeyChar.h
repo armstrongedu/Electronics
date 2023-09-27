45004454471c2af5be49dcdbe326b9c626bcc682
ayman988 <aymanemad222@gmail.com>
2023-09-27 18:14:41 +0200

    update keypad voltages'ranges

--- Lesson12(Keypad)/door_lock_system/KeyChar.h
+++ Lesson12(Keypad)/door_lock_system/KeyChar.h
@@ -4,43 +4,44 @@ based on three row resistors=2.7 Kilo-Ohm and two columns resistors=10 Kilo-Ohm 
 
 char getKey(){
   int value=analogRead(A0);
-  if(value>360&&value<370){
+  if(value>340&&value<370){
     return '1';
   }
-  else if(value>540&&value<550){
+  else if(value>500&&value<580){
     return '2';
   }
-  else if(value>1023){
+  else if(value>1020){
     return '3';
   } 
-  else if(value>330&&value<340){
+  else if(value>325&&value<340){
     return '4';
   } 
-  else if(value>479&&value<489){
+  else if(value>450&&value<500){
     return '5';
   }  
-  else if(value>856&&value<866){
+  else if(value>750&&value<900){
     return '6';
   } 
-  else if(value>308&&value<318){
+  else if(value>300&&value<325){
     return '7';
   } 
-  else if(value>429&&value<439){
+  else if(value>400&&value<450){
     return '8';
   }
-  else if(value>709&&value<719){
+  else if(value>650&&value<750){
     return '9';
   }
-  else if(value>286&&value<296){
+  else if(value>200&&value<300){
     return '*';
   }
-  else if(value>384&&value<394){
+  else if(value>370&&value<400){
     return '0';
   }
-  else if(value>600&&value<610){
+  else if(value>580&&value<650){
     return '#';
   }
   else{
     return '?';
   }
+  
 }
